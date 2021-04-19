7f6cd1f59989475a5bcaa9d8a639fcfc3465b0b7
shubham <shubham@shubhams-MacBook-Air.local>
2021-04-19 16:56:47 +0530

    added node server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static("build"));
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Listening on port: " + (process.env.PORT || 5000));
+});
+
+
+
